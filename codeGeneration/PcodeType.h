843197e5928a9a8c8787cd005cc739ad752f9031
闫东顺 <2019578140@qq.com>
2024-12-08 20:53:48 +0800

    change type.h

--- codeGeneration/PcodeType.h
+++ codeGeneration/PcodeType.h
@@ -11,86 +11,35 @@
 #include <unordered_map>
 
 enum  class CodeType {
-    LABEL,
-
-    // 声明一个变量，用于在程序中定义新的变量
-    VARINT, VARCHAR, VARINTARRAY, VARCHARARRAY,
-
-    // 将值数字、char压入栈顶
-    LDI, LDC,
-
-    PAL, //popValue then popAddress then setValue to address
-
-    // 加法操作，将两个值相加
-    ADD,SUB,
-
-    // 乘法操作，将两个值相乘
-    MUL,DIV,MOD,
-
-    // 逻辑非操作，取负
-    NOT,MUS,
-
-    // 主函数标签，用于标识程序的主函数开始位置
-    MAIN,
-
-    // 函数标签，用于标识一个函数的开始位置
-    FUNC,
-
-    // 函数返回，可以选择是否返回一个值
-    RET,
-
-    // 定义函数参数，包括参数名和参数类型
-    PARINT,PARCHAR,PARINTARRAY,PARCHARARRAY,
-
-    // 函数调用，调用指定名称的函数
-    CAL,
-
-    // 传递参数、传递函数参数
-    APR,  APA,
-
-    // 获取一个整数并将其压入栈顶
-    GETINT,
-
-    GETCHAR,
-
-    // 弹出栈中的值并打印，可能涉及字符串和参数数量
-    PRINTF,
-
-    // 获取变量的值，指定变量名和变量类型
-    LOD,
-
-    // 获取变量的地址，指定变量名和变量类型
-    LDA,
-
-    // 推送一些东西来占位
-    EMPTY,
-
-    //标记函数结束，用于无返回值的void函数
-    EXF,
-
-    // 退出程序
-    EXIT,
-
+    LABEL, //标签
+    VARINT, VARCHAR, VARINTARRAY, VARCHARARRAY, // 声明一个变量
+    LDI, LDC,// 将值数字、char压入栈顶
+    PAL,  //popValue then popAddress then setValue to address
+    ADD,SUB, //加减
+    MUL,DIV,MOD, //乘除、取模
+    NOT,MUS, //取否 取负
+    MAIN, // main标签
+    FUNC, // 函数标签
+    RET, //函数返回
+    PARINT,PARCHAR,PARINTARRAY,PARCHARARRAY,// 定义函数参数
+    CAL, // 函数调用，调用指定名称的函数
+    APR,  APA,    // 传递参数、传递数组参数
+    GETINT, GETCHAR, // 输入
+    PRINTF,// 输出
+    LOD, // 获取变量的值，指定变量名和变量类型
+    LDA,// 获取变量的地址，指定变量名和变量类型
+    EMPTY,// 给数组空间占位
+    EXF,//标记函数结束，用于无返回值的void函数
+    EXIT,// 退出程序
     JMP, //无条件跳转
-
     JPC, //如果栈顶为0，跳转
-
     JPF, //如果栈顶为非0，跳转
-
-    OR,
-
-    AND,
-
+    OR,AND, //逻辑运算
     EQL, //判断两个值是否相等
-
     NEQ, //判断两个值是否不相等
-
     LSS, //判断第一个值是否小于第二个值
-
     LER, //判断第一个值是否小于等于第二个值
-
     GRT, //判断第一个值是否大于第二个值
-
     GEQ, //判断第一个值是否大于等于第二个值
 };
 
